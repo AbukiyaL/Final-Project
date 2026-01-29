a65d29fbeb3af850922eeba52cc210d3a8d65dde
abukiya <highendcamry1@gmail.com>
2026-01-29 20:16:10 +0300

    Add form validation with vanilla JS

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,26 @@ const menuCont = document.querySelector('.menu-container');
 menuButton.addEventListener('click', () => {
     menuCont.classList.toggle('active');
 });
+const contactForm = document.querySelector('.contact-form');
+if(contactForm){
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    const firstName = document.getElementById('firstName').value.trim();
+    const lastName = document.getElementById('lastName').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if(firstName === "" || lastName === "" || email === "" || message === ""){
+        alert("please fill all the required fields marked with *");
+        return;
+    }
+     if (!email.includes("@") || !email.includes(".")) {
+    alert("Please enter a valid email address.");
+    return;
+     }
+
+    console.log("Form submitted Successfuly:", {firstName, lastName, email, message});
+    alert(`Thank you, ${firstName} ! your message has been sent.`);
+    contactForm.reset();
+});
+}
